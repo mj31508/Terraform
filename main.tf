e4ff391562f8223926cc77be16952e6321ab3be5
mj31508 <max.johnson@holbertonschool.com>
2021-07-13 15:51:34 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami           = "ami-40d28157"
+  instance_type = "t2.micro"
+
+  tags = {
+	Name = "terraform-example"
+ } 
+}
